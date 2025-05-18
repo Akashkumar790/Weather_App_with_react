b17df3c0985d5f0ba73dd86817e1ecabdcdca6ef
AKASH KUMAR <117560771+Akashkumar790@users.noreply.github.com>
2025-05-19 00:31:35 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,29 @@
+// import React from 'react'
+// import Weather from './components/Weather'
+
+// const App = () => {
+//   return (
+//     <div>App
+//       <Weather/>
+//     </div>
+    
+    
+//   )
+// }
+
+// export default App
+import React from 'react';
+import Weather from './components/Weather';
+
+const App = () => {
+  return (
+    <div>
+      App
+      <Weather />
+    </div>
+  );
+};
+
+export default App;
+
+
